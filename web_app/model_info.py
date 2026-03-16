ea523b0ba501d949f571b017e5f972bad29c0730
yflee <lee39.d.p@gmail.com>
2026-03-16 16:46:01 -0400

    add model info

--- web_app/model_info.py
+++ web_app/model_info.py
@@ -1,6 +1,16 @@
 import streamlit as st
 from state_helpers import initialize_session_state
+from utils import load_model
 initialize_session_state()
 
 # Title
-st.title("🏠 House Price Prediction App")
+st.title("Model Info")
+
+model = load_model()
+
+st.subheader("Model Structure")
+
+if hasattr(model, "_repr_html_"):
+    st.components.v1.html(model._repr_html_(), height=400, scrolling=True)
+else:
+    st.code(str(model))
