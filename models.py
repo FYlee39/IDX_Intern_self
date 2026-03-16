88e8638c13afb7e4220b496ab8f3b463cad005a6
yflee <lee39.d.p@gmail.com>
2026-03-16 13:42:33 -0400

    Compare model using new data

--- models.py
+++ models.py
@@ -23,3 +23,21 @@ from xgboost import XGBRegressor
 from xgboost import XGBClassifier
 from scipy.stats import randint, uniform, loguniform
 from sklearn.ensemble import StackingRegressor
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import glob
+import matplotlib.ticker as ticker
+import pickle
+import joblib
+from scipy import stats
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+from scipy.stats import randint, uniform, loguniform
